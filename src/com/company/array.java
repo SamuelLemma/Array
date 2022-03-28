5cfc505d5ada30613d6c40f946f0d85e20bcb827
Samuel <slemm@stu.vistula.edu.pl>
2022-03-29 00:56:42 +0200

    Initial commit

---
+++ src/com/company/array.java
@@ -0,0 +1,15 @@
+package com.company;
+
+        public class array {
+            public static void main(String[]args){
+                int tab[]= new int[10];
+                for(int i=0; i< tab.length; i++){
+                    tab[i]= tab.length-1-i;
+                }
+                System.out.println();
+                for (int x:tab){
+                    System.out.println("array["+ x +")=" + tab[x]);
+                }
+            }
+        }
+
